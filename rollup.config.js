bf1f8ea82bf5f25e4eaca07e5f3e23807d1870ec
Jan Kuri <jkuri88@gmail.com>
2017-01-24 20:01:09 +0100

    chore: make this work

--- rollup.config.js
+++ rollup.config.js
@@ -2,5 +2,8 @@ import ascii from "rollup-plugin-ascii";
 import node from "rollup-plugin-node-resolve";
 
 export default {
+  entry: 'index.js',
+  format: 'cjs',
+  dest: 'dist/topojson.js',
   plugins: [node(), ascii()]
 };
